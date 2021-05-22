0ad33258dba46f14868575990c3d0d2670da6491
Clarie Song <songvely17@gmail.com>
2021-05-22 17:42:28 +0900

    use __init__ not global variables

--- LeetCode/Top interview qestions/22. Generate Parentheses.py
+++ LeetCode/Top interview qestions/22. Generate Parentheses.py
@@ -1,25 +1,22 @@
 from collections import deque
 
-answers = []
-n = 0
-
-def recursive(cntLeft, cntRight, s, n):
-    print(s)
-    if cntLeft<cntRight or cntLeft>n or cntRight>n:
-        return
-    elif cntLeft == n and cntRight == n:
-        answers.append(s)
-    else:
-        recursive(cntLeft+1, cntRight, s+"(", n)
-        recursive(cntLeft, cntRight+1, s+")", n)
-
-
 class Solution(object):
+    
+    def __init__(self):
+        self.answers = []     # This is init for each test case.
+    
+    def recursive(self, cntLeft, cntRight, s, n):
+        if cntLeft<cntRight or cntLeft>n or cntRight>n:
+            return
+        elif cntLeft == n and cntRight == n:
+            self.answers.append(s)
+        else:
+            Solution.recursive(self, cntLeft+1, cntRight, s+"(", n)
+            Solution.recursive(self, cntLeft, cntRight+1, s+")", n)
+    
     def generateParenthesis(self, n):
         cntLeft = 1
         cntRight = 0
         s = "("
-        recursive(cntLeft, cntRight, s, n)
-        return answers
-
-print(Solution.generateParenthesis(0, 1))
+        Solution.recursive(self, cntLeft, cntRight, s, n)
+        return self.answers
